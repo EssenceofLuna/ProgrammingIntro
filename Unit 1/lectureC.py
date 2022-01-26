6f4126abf59c9edb1578dac16498d1ed289bf871
Alstro20 <sb74851@gmail.com>
2022-01-26 12:55:30 -0600

    asdasd

--- Unit 1/lectureC.py
+++ Unit 1/lectureC.py
@@ -2,15 +2,25 @@
 # CIT 1113 Lecture C
 # 1/26/2022
 
-print("How many time to repeat?")
+# print("How many time to repeat?")
 
+# try:
+#     repeats = int(input())
+# except:
+#     print("Error: invalid input")
+#     quit()
+
+# number = 1
+# for x in range(repeats):
+#     print(x, number)
+#     number = number * 2
+
+print("How old are you?")
 try:
-    repeats = int(input())
+    age = int(input())
 except:
     print("Error: invalid input")
     quit()
 
-number = 1
-for x in range(repeats):
-    print(x, number)
-    number = number * 2
+months = age * 12
+print("You are at least", months, "months old.")
