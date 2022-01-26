173a5d64908d89b29bfd2d055bb7fab9885a6db4
Alstro20 <sb74851@gmail.com>
2022-01-26 13:11:34 -0600

    renamed firstclass.py to lectureA.py

---
+++ Unit 1/lectureA.py
@@ -0,0 +1,12 @@
+# cit1113
+# Lecture o.
+# Alex Brown
+
+#print("Hello World!")
+
+#1 input name
+print("What is your name?")
+name = input()
+
+#2 display Hello, name
+print("Hello, " + name + "!")
