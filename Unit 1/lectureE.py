4d1d821c8e220aa75b7167a42cf9a80664c2fb0e
Alstro20 <sb74851@gmail.com>
2022-02-07 12:54:05 -0600

    asdasd

--- Unit 1/lectureE.py
+++ Unit 1/lectureE.py
@@ -2,5 +2,43 @@
 # Alex Brown
 # Lecture E
 
-# hexed.it - view hexadecimal code
+# Lecture notes:
+"""
+hexed.it - view hexadecimal code
 
+input - things coming in
+output - things going out
+application - does a thing
+"""
+
+
+
+# pseudocode
+"""
+start
+    input myNumber
+    set myAnswer = myNumber * 2
+    output myAnswer
+stop
+"""
+# in-class code
+"""
+print("Enter a number to double:")
+myNumber = float(input(">"))
+
+myAnswer = myNumber * 2
+
+print("Doubled number:", myAnswer)
+"""
+
+# My enhanced code
+try:
+    print("Enter a number to double:")
+    myNumber = float(input(">"))
+except:
+    print("Error: invalid number entered.")
+    quit()
+
+myAnswer = myNumber * 2
+
+print("Doubled number:", myAnswer)
