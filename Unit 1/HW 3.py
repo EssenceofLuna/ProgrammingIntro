10d0dbd3367ef4876dcaeb97d63a235a804ea4ae
Alstro20 <sb74851@gmail.com>
2022-02-09 13:37:33 -0600

    asdasd

---
+++ Unit 1/HW 3.py
@@ -0,0 +1,28 @@
+# cit1113
+# Alex Brown
+# Homework 3
+
+# Haiku:
+# This is a haiku
+# I don't know how to end it
+# Refrigerator
+
+# Method 1
+
+print("This is a haiku")
+print("I don't know how to end it")
+print("Refrigerator")
+
+
+# Method 2
+print("""This is a haiku
+I don't know how to end it
+Refrigerator""")
+
+
+# Method 3
+haiku = """This is a haiku
+I don't know how to end it
+Refrigerator
+"""
+print(haiku)
--- Unit 1/lectureF.py
+++ Unit 1/lectureF.py
@@ -7,3 +7,5 @@
 functions
 fruitfull function - does something, returns something
 """
+
+print("line 1\nline 2\n\nLine 4")
