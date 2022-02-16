de564be0cee88226982f7169e2df8ffd287ec72d
Alstro20 <sb74851@gmail.com>
2022-02-16 12:37:24 -0600

    asd

---
+++ Unit 2/lectureH.py
@@ -0,0 +1,14 @@
+# cit1113
+# Alex Brown
+# Lecture G
+
+"""
+# Pseudocode that calculated volume of a cube given an edge
+
+start
+    output "enter edge length of cube"
+    input edge
+    set volume = edge ** 3
+    output "Volume of cube with edge length", edge, "is", volume
+end
+"""
