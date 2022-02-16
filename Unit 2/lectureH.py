bf9c19fead24f272d08e30d6368d1c46bb741bfb
Alstro20 <sb74851@gmail.com>
2022-02-16 12:51:59 -0600

    asdasd

--- Unit 2/lectureH.py
+++ Unit 2/lectureH.py
@@ -3,12 +3,33 @@
 # Lecture G
 
 """
-# Pseudocode that calculated volume of a cube given an edge
+# Pseudocode that calculates volume of a cube given an edge length
 
 start
-    output "enter edge length of cube"
+    output "Enter edge length of cube"
     input edge
     set volume = edge ** 3
     output "Volume of cube with edge length", edge, "is", volume
 end
 """
+
+
+
+
+def cubeVolume(edge = None):
+    # Python function that calculated volume of a cube given an edge length
+    if (edge==None):
+        # If no edge value is given, the function will prompt the user for an edge length
+        print("Enter edge length of cube")
+        edge = float(input(">"))
+
+    volume = edge ** 3
+    return volume
+
+def main():
+    # Main function
+    print(cubeVolume())
+
+if __name__ == "__main__":
+    # Run main if being run as script
+    main()
