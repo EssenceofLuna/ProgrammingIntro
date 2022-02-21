cc4b4ed374c4c681bd775cf2f09f92cb45dc8b2c
Alstro20 <sb74851@gmail.com>
2022-02-21 13:15:50 -0600

    asasd

--- Unit 2/lectureI.py
+++ Unit 2/lectureI.py
@@ -4,3 +4,19 @@
 
 """
 """
+
+# Donut code
+
+print("Hungry? (Y/N)")
+response = input(">")
+
+if response == "Y":
+    donuts = 12
+    while donuts > 0:
+        print("Let's eat a donut!")
+        donuts = donuts - 1
+        print("Donuts remaining: ", donuts)
+else:
+    print("Ok, no donuts for you")
+
+print("Done")
