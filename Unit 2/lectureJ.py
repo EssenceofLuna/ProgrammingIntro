c03f76d4be8051ff4a8a40efac57866793bd4a21
Alstro20 <sb74851@gmail.com>
2022-02-28 13:39:02 -0600

    asd

---
+++ Unit 2/lectureJ.py
@@ -0,0 +1,14 @@
+# cit1113
+# Alex Brown
+# Lecture J
+
+
+"""
+Use logical structures
+No spaghetti code
+Python does not have "goto" statements
+
+HW: Draw some flowchart
+
+Extra credit: convert to python
+"""
