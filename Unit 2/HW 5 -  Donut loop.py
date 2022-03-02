65c4cce4d7cf2c99be57f69dc6a18a0609a6fe82
Alstro20 <sb74851@gmail.com>
2022-03-02 13:54:02 -0600

    asd

---
+++ Unit 2/HW 5 -  Donut loop.py
@@ -0,0 +1,13 @@
+print("Hungry? (Y/N)")
+response = input(">")
+
+if response == "Y":
+    donuts = 12
+    while donuts > 0:
+        print("Let's eat a donut!")
+        donuts = donuts - 1
+        print("Donuts remaining:", donuts)
+else:
+    print("Ok, no donuts for you")
+
+print("Done")
