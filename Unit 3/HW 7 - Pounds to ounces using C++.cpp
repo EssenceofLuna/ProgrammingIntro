0d722605152fff42dfc4a6a28cd1dc5493c640be
Alstro20 <sb74851@gmail.com>
2022-03-02 14:19:21 -0600

    asd

---
+++ Unit 3/HW 7 - Pounds to ounces using C++.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//Original code: https://onlinegdb.com/7A0U2O-4I
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double lbs, ounces;
+    cout << "Convert lbs to ounces" << endl;
+    cout << "How many lbs? >";
+    cin >> lbs;
+    ounces = lbs * 16;
+    cout << "That equals " << ounces << " ounces" << endl;
+
+    return 0;
+}
+
