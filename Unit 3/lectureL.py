3b1e90e4f4ddc108a3bdbe23c157a0bf3064b7d4
Alstro20 <sb74851@gmail.com>
2022-03-07 13:42:55 -0600

    yes

---
+++ Unit 3/lectureL.py
@@ -0,0 +1,28 @@
+# cit1113
+# Alex Brown
+# Lecture L
+
+"""
+book says to avoid using !=
+nested if statements or AND keyword
+OR keyword will skip second check if first is true. Put more likely to be true first for optimization.
+
+boolean is set to either true or false. Sometimes called a flag. Can be tested with if or while without == True
+
+not (a and b)
+same as
+not a and not b
+
+not (a or b)
+same as
+not a and not b
+"""
+
+frozen = True
+if frozen:
+    print("I'm frozen")
+
+steamy = False
+if steamy:
+    print("I'm steamy")
+
