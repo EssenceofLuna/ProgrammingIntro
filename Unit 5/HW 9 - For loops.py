f4b5994f6897a39d8aa4ea8c6e39dd10d7d57284
Alstro20 <sb74851@gmail.com>
2022-04-07 16:06:12 -0500

    asd

---
+++ Unit 5/HW 9 - For loops.py
@@ -0,0 +1,22 @@
+# A. Count from 5 to 10 by ones
+
+for x in range(5, 11, 1):
+    print(x)
+
+
+# B. Count from 1 to 5 by ones
+
+for x in range(1, 6, 1):
+    print(x)
+
+
+# C. Count from 0 to 5 by ones
+
+for x in range(0, 6, 1):
+    print(x)
+
+
+# D. Count from 10 to 50 by tens
+
+for x in range(10, 51, 10):
+    print(x)
