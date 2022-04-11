947aa28b4d5eb7006ee31964838438bccf67fac9
Alstro20 <sb74851@gmail.com>
2022-04-11 13:38:34 -0500

    yfgd

--- Week 12/lectureS.py
+++ Week 12/lectureS.py
@@ -3,5 +3,22 @@
 # Lecture S
 
 """
+Arrays and Lists
 
+Lists can be expanded, arrays can't
+Lists cn hold multiple types
+Python uses lists
 """
+
+kids = ["Alice", "Becky", "Carol"]
+for v in kids:
+    print("Kid: "+v)
+
+kids[0] = "Ann"
+kids[1] = "Bob"
+kids[2] = "Chuck"
+kids.append("Beans")
+
+for v in kids:
+    print("Kid: "+v)
+
