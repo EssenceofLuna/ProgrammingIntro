b6e8ac2c5b46d0e6898763d1584614630f6f9fc6
Alstro20 <sb74851@gmail.com>
2022-04-12 12:57:43 -0500

    hw 13

---
+++ Week 12/HW 13 - Array of movies.py
@@ -0,0 +1,9 @@
+movies = ["Morbius", "Ambulance", "Sonic the Hedgehog 2", "The Batman", "Uncharted", "Dog"]
+
+print("For value loop:")
+for m in movies:
+    print("Movie:", m)
+
+print("for index in range loop:")
+for i in range(0, len(movies)):
+    print("Movie:", movies[i])
