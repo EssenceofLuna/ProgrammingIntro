a2393f10a75ac58ebd3952dc7e83e5358e81a222
Alstro20 <sb74851@gmail.com>
2022-04-13 13:00:26 -0500

    asd

---
+++ Week 12/lectureT.py
@@ -0,0 +1,15 @@
+# cit1113
+# Alex Brown
+# Lecture T
+
+"""
+Parallel arrays
+Same length, items are linked by subscript #
+"""
+
+a1 = ["red", "blue", "green"]
+a2 = ["apples", "berries", "beans"]
+a3 = [1.99, .99, 2.99]
+
+for i in range(len(a1)):
+    print(a1[i], a2[i], a3[i])
