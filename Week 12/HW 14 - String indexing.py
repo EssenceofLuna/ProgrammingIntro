942fde7f884a3a81827f50f2b7e1cc6b7b026dfe
Alstro20 <sb74851@gmail.com>
2022-04-19 12:30:05 -0500

    asd

--- Week 12/HW 14 - String indexing.py
+++ Week 12/HW 14 - String indexing.py
@@ -2,10 +2,19 @@
 # the capital letters in this sentence
 # fragment.  The first two are done for you.
 
-# print(s[0]) # W
-# print(s[5]) # I
 
 s = "When In The Course Of Human Events It Becomes Necessary"
 
-for upper in [char for char in s if char.isupper()]:
-    print(upper)
+# for upper in [char for char in s if char.isupper()]:
+#     print(upper)
+
+print(s[0]) # W
+print(s[5]) # I
+print(s[8])
+print(s[12])
+print(s[19])
+print(s[22]) # H
+print(s[28])
+print(s[35])
+print(s[38]) # B
+print(s[46]) # N
