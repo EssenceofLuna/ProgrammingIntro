9162bb5a24afcedc2c02417c1afca6819ccd0e1f
Alstro20 <sb74851@gmail.com>
2022-04-20 14:44:06 -0500

    sdf

---
+++ Week 13/HW 15 - String slicing.py
@@ -0,0 +1,21 @@
+# Demonstrate your knowledge of string slicing by
+# printing each word using slicing. 
+
+# The first two are done for you.
+
+s = "When In The Course Of Human Events It Becomes Necessary"
+
+# Alternate for loop solution
+# for word in s.split():
+#     print(word)
+
+print(s[0:4])    # When
+print(s[5:7])    # in
+print(s[8:11])
+print(s[12:18])
+print(s[19:21])
+print(s[22:27])
+print(s[28:34])
+print(s[35:37])
+print(s[38:45])
+print(s[46:])    # Necessary
